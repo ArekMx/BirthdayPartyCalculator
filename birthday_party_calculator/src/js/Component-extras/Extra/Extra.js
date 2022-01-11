cf57cebe11a0db980045e7f4b7d92f8af9f1228b
ArekMx <arkadiuszmartyniuk@interia.pl>
2022-01-11 14:00:31 +0100

    Btns extras modifed

--- birthday_party_calculator/src/js/Component-extras/Extra/Extra.js
+++ birthday_party_calculator/src/js/Component-extras/Extra/Extra.js
@@ -1,5 +1,7 @@
+import React, {useState, useEffect} from "react"
 
-export const Extra =({price, name, className}) => {
+
+export const Extra =({price, name, className, onAdd}) => {
     
     // const styleOnClick =(x) =>{
     //     return parseFloat(currentPrice) === price ? x : null
@@ -7,18 +9,27 @@ export const Extra =({price, name, className}) => {
 
     // const boxShadowStyle = "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)"
 
+    const [colorBtn, setColorBtn] = useState(false);
+
+    const handleAddAddition =(event) => {
+
+        if(typeof onAdd !== "function" ) {
+            return
+        }
+        onAdd(event.target.value, price);
+
+        setColorBtn(true);
+
+    }
+
+
     console.log(name);
     return (
             <div>
                 <button
-                // value={price}
-                // onClick={event => onAdd(event.target.value)}
-                // style={{
-                //         backgroundColor: styleOnClick("purple"),
-                //         color: styleOnClick("yellow"),
-                //         transform: styleOnClick("none"),
-                //         boxShadow: styleOnClick(boxShadowStyle)
-                //     }}
+                value={price}
+                onClick={handleAddAddition}
+                // style={{color: colorBtn ? "gold" : "#6a1e50", backgroundColor: colorBtn ? "#6a1e50" : "gold"}}
                 className={className}>{price} zł - {name}</button>
             </div>
     )

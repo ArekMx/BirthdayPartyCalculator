2440ac4f6736610ad9209be78b83f6b78cbc1143
ArekMx <arkadiuszmartyniuk@interia.pl>
2022-05-12 07:20:45 +0200

    style change 22

--- birthday_party_calculator/src/js/Component-table-select/functions.js
+++ birthday_party_calculator/src/js/Component-table-select/functions.js
@@ -8,18 +8,18 @@ export const countTotalTableSelect= (numberOfKids, pricePerKid, setTotal, setInf
     if (numberOfKids > 9 && pricePerKid === "38") {
         setTotal((numberOfKids * pricePerKid) - pricePerKid);
         setInfo(true);
-        setText("Promocja! Jubilat gratis! Nie płacisz za JEDNO dziecko!");
+        setText("Promocja! Od poniedziałku do piątku Jubilat gratis! Nie płacisz za JEDNO dziecko!");
         if (numberOfKids > 21) {
           setTotal((numberOfKids * pricePerKid) - pricePerKid*2);
-          setText("Promocja! Jubilat i Gość gratis! Nie płacisz za DWÓJKĘ dzieci!");
+          setText("Promocja! Od poniedziałku do piątku Jubilat i Gość gratis! Nie płacisz za DWÓJKĘ dzieci!");
         }
       } else if (numberOfKids > 9 && pricePerKid === "42") {
         setTotal((numberOfKids * pricePerKid) - pricePerKid);
         setInfo(true);
-        setText("Promocja! Jubilat gratis! Nie płacisz za JEDNO dziecko!");
+        setText("Promocja! Od poniedziałku do piątku Jubilat gratis! Nie płacisz za JEDNO dziecko!");
         if (numberOfKids > 21) {
           setTotal((numberOfKids * pricePerKid) - pricePerKid*2);
-          setText("Promocja! Jubilat i Gość gratis! Nie płacisz za DWÓJKĘ dzieci!");
+          setText("Promocja! Od poniedziałku do piątku Jubilat i Gość gratis! Nie płacisz za DWÓJKĘ dzieci!");
         }
       } else {
         setTotal(numberOfKids * pricePerKid);
